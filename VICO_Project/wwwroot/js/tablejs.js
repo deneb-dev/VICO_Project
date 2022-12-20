b8fa5e223885c8ef1d9648118bedeea20e2dc93e
Hendri <rickyhendry05@gmail.com>
2022-12-20 17:47:21 +0800

    1.6

--- VICO_Project/wwwroot/js/tablejs.js
+++ VICO_Project/wwwroot/js/tablejs.js
@@ -1,30 +1,8 @@
 $(document).ready(function () {
-    $('#idTable').DataTable({
-        "ajax": {
-            /*url: "https://pokeapi.co/api/v2/pokemon/",*/
-            dataSrc: 'results',
-            dataType: "JSON"
-        },
+    $('idTableCuti').DataTable({
         dom: 'Bfrtip',
         buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        "columns": [{
-            "data": "id",
-            "render": function (data, type, row, meta) {
-                return meta.row + meta.settings._iDisplayStart + 1;
-            }
-        },
-        {
-            "data": "name"
-        },
-        {
-            "data": "url",
-            "render": function (data, type, row) {
-                return `<button class="btn btn-primary" onclick="detailModal('${data}')" data-bs-toggle="modal" data-bs-target="#modalDetailPoke">Detail</button>`;
-            }
-        }
         ]
     });
-
 });

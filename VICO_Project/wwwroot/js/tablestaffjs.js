efd4b1d5c3532ffdd2256d1f6045467ad934a08f
Hendri <rickyhendry05@gmail.com>
2022-12-20 18:01:54 +0800

    1.8

---
+++ VICO_Project/wwwroot/js/tablestaffjs.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('idTableStaffCuti').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+});

c3f187a3990295db4e6b8aacf0c0ac864ee80933
MATRIX <MATRIX@LAPTOP-7FOH7BBK>
2022-12-22 23:20:37 +0800

    3.8

---
+++ VICO_Project/Controllers/myAction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace VICO_Project.Controllers
+{
+    public class myAction : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+;        }
+    }
+}

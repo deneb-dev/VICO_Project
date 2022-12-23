130215046da81515ba1145507dd9d1222e9a6a33
MATRIX <MATRIX@LAPTOP-7FOH7BBK>
2022-12-23 18:00:23 +0800

    4.8

--- VICO_Project/ViewModels/Register.cs
+++ VICO_Project/ViewModels/Register.cs
@@ -10,6 +10,6 @@ namespace VICO_Project.ViewModels
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int RoleId { get; set; } = 1;
+        public int RoleId { get; set; } = 2;
     }
 }
